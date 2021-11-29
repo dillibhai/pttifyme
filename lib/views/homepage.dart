55de1e571ec63c26bbabee59cd450e9ac6b4400d
Dilli Bhandari <bhandaridilli1@gmail.com>
2021-11-29 23:27:26 +0545

    add some parts.

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:pttifyme/constraints/constants.dart';
@@ -8,28 +9,64 @@ class HomePage extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: ColorUtils.primarycolor,
-        // centerTitle: true,
-        title: Text(
-          'PTTify Me',
-          style: TextStyle(
-            fontStyle: FontStyle.italic,
-            color: ColorUtils.black,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: ColorUtils.primarycolor,
+          // centerTitle: true,
+          title: Text(
+            'PTTify Me',
+            style: TextStyle(
+              fontStyle: FontStyle.italic,
+              color: ColorUtils.black,
+            ),
           ),
+          leading: IconButton(
+              onPressed: () {},
+              icon: Image.asset(
+                'assets/images/hamburger.png',
+                height: 20,
+                width: 20,
+              )),
+          actions: [tabbtns()],
         ),
-        leading: IconButton(
-            onPressed: () {},
-            icon: Image.asset(
-              'assets/images/hamburger.png',
-              height: 20,
-              width: 20,
-            )),
-        actions: [tabbtns()],
-      ),
-      body: _msgdata(),
-    );
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                margin: EdgeInsets.only(left: 16, top: 7),
+                child: const Text(
+                  "Recent PTT",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 15, bottom: 10),
+                child: _imgdata(),
+                height: 87,
+                width: double.infinity,
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 20, bottom: 10),
+                child: Text(
+                  "Recent Chat",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+              ),
+              SingleChildScrollView(
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 50),
+                  height: MediaQuery.of(context).size.height / 1.5,
+                  width: double.infinity,
+                  child: _msgdata(),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 
   Widget tabbtns() {
@@ -49,7 +86,6 @@ class HomePage extends GetView<HomeController> {
   Widget _msgdata() {
     return controller.obx(
       (data) => ListView.builder(
-          padding: const EdgeInsets.all(8),
           itemCount: data!.length,
           itemBuilder: (BuildContext context, int index) {
             return Column(
@@ -81,7 +117,7 @@ class HomePage extends GetView<HomeController> {
                   color: Colors.grey,
                 ),
                 const SizedBox(
-                  height: 10,
+                  height: 15,
                 )
               ],
             );
@@ -91,4 +127,35 @@ class HomePage extends GetView<HomeController> {
       ),
     );
   }
+
+  Widget _imgdata() {
+    return controller.obx(
+      (data) => ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: data!.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Row(
+              children: <Widget>[
+                Column(
+                  children: [
+                    CircleAvatar(
+                      radius: 30,
+                      backgroundImage:
+                          NetworkImage(data[index]['picture']['large']),
+                    ),
+                    Text(data[index]['name']['first'])
+                  ],
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+              ],
+            );
+          }),
+      onError: (error) => Center(
+        child: Text(error.toString()),
+      ),
+    );
+  }
 }
+//bhsg hjgshj sjhghs
